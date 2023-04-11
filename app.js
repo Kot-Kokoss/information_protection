03e1fecf82c548ddce7ba581dd7c3a63a5f3f74b
Илья Заостровский <112894177+Kot-Kokoss@users.noreply.github.com>
2023-04-11 09:34:27 +0500

    Password generator
    Generates a specified number of passwords with the resulting long. After creating passwords, encrypts them and saves them in a separate file.

---
+++ app.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const readline = require('readline-sync');
+const fs = require('fs');
+
+const random = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const pass_lenth = readline.question("Enter the password length - ");
+let pass_number = readline.question("Enter the number of passwords - ");
+const encrypted_step = readline.question("Enter the encryption step - ");
+const alphabet = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789%#-&$';
+
+fs.open('password_storage.txt', 'a', (err) => {
+  if(err) throw err;
+  console.log('File created');
+});
+
+while (pass_number != 0) {
+  pass_number -= 1;
+
+  let password = '',
+    characters_left = pass_lenth;  
+
+  while (characters_left != 0) {
+    password += alphabet[random(0, 66)];
+    characters_left -= 1;
+  };
+
+  console.log(password);
+  let encrypted_password;
+
+  for (let i = 0; i != pass_lenth; i++) {
+    let pass_element = password[i],
+        pass_element_index = alphabet.indexOf(pass_element) + encrypted_step;
+    if (pass_element_index > 66) {
+      pass_element_index -= 67;
+    }
+    encrypted_password += alphabet[pass_element_index];
+  }
+
+  //write pass in txt-file
+  fs.appendFile('password_storage.txt', encrypted_password + '\n', (err) => {
+    if(err) throw err;
+    console.log('Data has been added!');
+});
+};
