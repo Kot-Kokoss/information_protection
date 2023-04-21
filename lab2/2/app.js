74cedf3eacd77251ab019b06e5edf545e7a5610c
Kot-Kokoss <izaostrovskiy@mail.ru>
2023-04-21 14:49:18 +0500

    working

--- lab2/2/app.js
+++ lab2/2/app.js
@@ -2,58 +2,48 @@
 
 const readline = require('readline-sync');
 const fs = require('fs');
+const iconv = require('iconv-lite');
 let fileContent = fs.readFileSync('text.txt', 'utf-8');
 
-let encr_or_decr = readline.question('For encryption, enter 1, for decryption, enter 2 - ');
 let text = readline.question('Enter the text or press 1 to read the text from the file - ');
 let encrypted_key = (readline.question("Enter the encryption key - "));
-const alphabet = '#ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 if (text == 1) {
     text = fileContent;
 };
+
+console.log('Original message - ', text)
 
 if (text.length > encrypted_key.length) {
 
     while (text.length > encrypted_key.length) {
         encrypted_key += encrypted_key;
     };
+
+    let tail = encrypted_key.length - text.length;
+    encrypted_key = encrypted_key.slice(0, -tail);
     
-    console.log(encrypted_key);
+    console.log('Encrypted key - ', encrypted_key);
 };
 
-if (encr_or_decr == 1) {
-    let encrypted_text = '',
-        text_element_encrypted;
-
+let encrypted_text = '';
+        
     for (let i = 0; i != text.length; i++) {
-        let text_element = text[i],
-            sum_mod = alphabet.indexOf(text_element) + alphabet.indexOf(encrypted_key[i]);  
-        if (sum_mod >= 27) {
-            text_element_encrypted = alphabet[sum_mod - 27];
-        } else {
-            text_element_encrypted = alphabet[sum_mod];
+        let char_text = String(text[i]),
+            char_key = String(encrypted_key[i]);
+        let buffer_text = iconv.encode(char_text, 'cp1251'),
+            buffer_key = iconv.encode(char_key, 'cp1251');
+        let code_text = buffer_text[0],
+            code_key = buffer_key[0];
+        let encrypted_code = code_text + code_key;
+        
+        if (encrypted_code > 255) {
+            encrypted_code -= 256;
         };
 
-        encrypted_text += text_element_encrypted;
+        let bytes = new Uint8Array([encrypted_code]);
+        let encrypted_element = iconv.decode(Buffer.from(bytes), 'cp1251');
+        encrypted_text += encrypted_element;
     };
 
-    console.log(encrypted_text);
-};
-
-if (encr_or_decr == 2) {
-    let decrypted_text = text;
-        
-    // for (let i = 0; i != text.length; i++) {
-    //     let text_element = text[i],
-    //         text_element_index = text.indexOf(text_element) - encrypted_step;  
-        
-    //     if (text_element_index < 0) {
-    //         text_element_index = text.length - (encrypted_step - Math.abs(0 - text.indexOf(text_element)));
-    //     };
-    //     decrypted_text += text[text_element_index];
-    // };
-
-    console.log(decrypted_text);
-}
-
+    console.log('Encrypted message - ', encrypted_text);
