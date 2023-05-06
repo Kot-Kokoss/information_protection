9cddfeacc42c6f7740b4c26b04f0fdf6eb90dfc2
Илья Заостровский <112894177+Kot-Kokoss@users.noreply.github.com>
2023-05-06 20:31:12 +0500

    Cardan latice
    Encryption and decryption using a cardan lattice.

--- lab3/2/app.js
+++ lab3/2/app.js
@@ -3,12 +3,18 @@
 const readline = require('readline-sync');
 const fs = require('fs');
 const iconv = require('iconv-lite');
+const { reverse } = require('dns');
 const id_list = new Map();
 
 let matrix_size = readline.question('Enter matrix size X*X, x - '),
     text = readline.question('Enter the text - '),
-    matrix = [],
-    matrix_mask = [[1,0,0,1,1,0],
+    matrix = [[0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0]],
+    matrix_mask = [[1,0,0,0,1,0],
                    [0,1,0,0,0,0],
                    [0,0,1,0,1,0],
                    [0,0,0,0,0,1],
@@ -22,36 +28,75 @@ if (Math.floor(text.length / matrix_size) < text.length / matrix_size) {
     };
 };
 
-for (let i = 1; i != 4; i ++) {
-
-    // for (let i = 0; i < matrix_size; i ++) {
-    //     let matrix_string = [];
-    //     for (let j = 0; j < matrix_size; j ++) {
-    //         if (matrix_mask[i][j] == 1) {
-    //             matrix_string.push(text[0]);
-    //             text = text.slice(1);
-    //         } else {
-    //             matrix_string.push(0);
-    //         }
-    //     }
-    
-    //     matrix.push(matrix_string);
-    // }
-    console.log(matrix_mask);
-
-    let new_matrix_mask = matrix_mask;
+//Encryption
+for (let i = 0; i != 4; i ++) {
 
     for (let i = 0; i < matrix_size; i ++) {
+
         for (let j = 0; j < matrix_size; j ++) {
-            new_matrix_mask[j][matrix.size - 1 - i] = matrix_mask[i][j];
+            if (matrix_mask[i][j] == 1) {
+                matrix[i][j] = text[0];
+                text = text.slice(1);
+            } 
         }
     }
-    
-    // console.log(matrix);
+    console.log(matrix_mask)
+    let new_matrix_mask = [];
+
+    for (let i = 0; i < matrix_size; i++) {
+        new_matrix_mask.push([]);
+        for (let j = 0; j < matrix_size; j++) {
+          new_matrix_mask[i].push(matrix_mask[matrix_size - j - 1][i]);
+        }
+      }
+
+    matrix_mask = new_matrix_mask
+    console.log(matrix);
+    console.log('Remaining text -', text);
 }
 
-// let matrix_string = [];
-// for (let i = 0; i < matrix_size; i ++) {
-//     matrix_string.push(0);
-// }
+let encrypted_text = '';
 
+for (let i = 0; i < matrix_size; i++) {
+    for (let j = 0; j < matrix_size; j++) {
+        encrypted_text += matrix[i][j];
+    }
+}
+
+console.log('Encrypted text -', encrypted_text);
+
+//Decryption
+for (let i = 0; i < matrix_size; i++) {
+    for (let j = 0; j < matrix_size; j++) {
+        matrix[i][j] = encrypted_text[0];
+        encrypted_text = encrypted_text.slice(1);
+    }
+}
+
+for (let i = 0; i != 4; i ++) {
+    let new_matrix_mask = [];
+
+    for (let i = 0; i < matrix_size; i++) {
+        new_matrix_mask.push([]);
+        for (let j = 0; j < matrix_size; j++) {
+          new_matrix_mask[i].push(matrix_mask[j][matrix_size - i - 1]);
+        }
+      }
+
+    matrix_mask = new_matrix_mask
+    console.log(matrix_mask);
+
+    for (let i = 5; i > -1; i--) {
+        for (let j = 5; j > -1; j--) {
+            if (matrix_mask[i][j] == 1) {
+                text += matrix[i][j];
+            } 
+        }
+    }
+
+    console.log('Decrypted part -', text);
+}
+
+let reversed_text = text.split("").reverse().join("");
+
+console.log('Decrypted text -', reversed_text);
